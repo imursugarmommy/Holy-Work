cbaff59434ebe1325292d624f78205028082d4dd
imursugarmommy <139384471+imursugarmommy@users.noreply.github.com>
2023-09-10 21:03:41 +0200

    Copied from tutorial, fun anyways
    I like it

---
+++ Rock paper scissors full project + autoplay/app.js
@@ -0,0 +1,158 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+updateScoreElement();
+
+/*
+if (!score) {
+  score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+  };
+}
+*/
+
+let isAutoPlaying = false;
+let intervalId;
+
+function resetScore() {
+  document.querySelector('.yes').addEventListener('click', () => {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+    document.querySelector('.confirm').classList.remove('toggle');
+  });
+
+  document.querySelector('.no').addEventListener('click', () => {
+    document.querySelector('.confirm').classList.remove('toggle');
+  });
+}
+
+function autoplay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    document.querySelector('.auto-play').innerHTML = 'Stop Play';
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    document.querySelector('.auto-play').innerHTML = 'Auto Play';
+    isAutoPlaying = false;
+  }
+}
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', () => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  } else if (event.key === 'a') {
+    autoplay();
+  } else if (event.key === 'Backspace') {
+    resetScore();
+  }
+});
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+
+  let result = '';
+
+  if (playerMove === 'scissors') {
+    if (computerMove === 'rock') {
+      result = 'You lose.';
+    } else if (computerMove === 'paper') {
+      result = 'You win.';
+    } else if (computerMove === 'scissors') {
+      result = 'It is a tie.';
+    }
+  } else if (playerMove === 'paper') {
+    if (computerMove === 'rock') {
+      result = 'You win.';
+    } else if (computerMove === 'paper') {
+      result = 'It is a tie.';
+    } else if (computerMove === 'scissors') {
+      result = 'You lose.';
+    }
+  } else if (playerMove === 'rock') {
+    if (computerMove === 'rock') {
+      result = 'It is a tie.';
+    } else if (computerMove === 'paper') {
+      result = 'You lose.';
+    } else if (computerMove === 'scissors') {
+      result = 'You win.';
+    }
+  }
+
+  if (result === 'You win.') {
+    score.wins = score.wins += 1;
+  } else if (result === 'You lose.') {
+    score.losses = score.losses += 1;
+  } else if (result === 'It is a tie.') {
+    score.ties = score.ties += 1;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));
+
+  updateScoreElement();
+
+  document.querySelector('.js-result').innerHTML = result;
+
+  document.querySelector('.js-moves').innerHTML = `You 
+  <img src="${playerMove}-emoji.png" alt="">
+  <img src="${computerMove}-emoji.png" alt="">
+  Computer`;
+}
+
+function updateScoreElement() {
+  document.querySelector(
+    '.js-score'
+  ).innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = 'rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = 'paper';
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = 'scissors';
+  }
+
+  return computerMove;
+}
+
+document.querySelector('.reset-btn').addEventListener('click', () => {
+  document.querySelector('.confirm').classList.add('toggle');
+  resetScore();
+});
+
+document.querySelector('.auto-play').addEventListener('click', () => {
+  autoplay();
+});
